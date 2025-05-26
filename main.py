015c97b71ffcf209d0446f1442c792369af9aa72
VanoBond <wholeelottaredd@gmail.com>
2025-05-26 21:16:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww26540\viewh14200\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 echo "# \uc0\u1052 \u1086 \u1081  \u1087 \u1088 \u1086 \u1077 \u1082 \u1090  \u1085 \u1072  GitHub" > README.md\
+echo "print('Hello, World!')" > main.py}
